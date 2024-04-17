8dfadf50754bf740856e7ccd1888ad7cb3fbe25b
Jeff Siarto <jsiarto@gmail.com>
2024-04-17 16:01:11 +0000

    Basic Howler.js player

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add JavaScript below
+// Initialize a new MP3
+const chopin = new Howl({
+  src: ['chopin.mp3']
+});
+
+// Setup the audio controls
+const playButton = document.getElementById('play');
+const pauseButton = document.getElementById('pause');
+const stopButton = document.getElementById('stop');
+
+// Attached listeners to the buttons and map to appropriate Howl methods
+playButton.addEventListener('click', () => {
+  chopin.play();
+})
+
+pauseButton.addEventListener('click', () => {
+  chopin.pause();
+})
+
+stopButton.addEventListener('click', () => {
+  chopin.stop();
+})
